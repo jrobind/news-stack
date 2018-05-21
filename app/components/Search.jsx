9164d723e7544b7b3a5668df8504aa91be143bad
James Robinson <jrobind@outlook.com>
2018-05-21 17:19:41 +0100

    Search is now a stateless functional component

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,43 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from '../styles/components/Search.scss';
 
-class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            search: ''
-        }
-        
-        this.handleInput = this.handleInput.bind(this);
-        this.handleSubmission = this.handleSubmission.bind(this);
-    }
-    
-    handleInput(e) {
-        const val = e.target.value;
-        this.setState(() => ({search: val}));
-    }
-    
-    handleSubmission() {
-        const { search } = this.state;
-        console.log(search);
-    }
-    
-    render() {
-        return (
-            <div className={styles.container}>
-                <input 
-                    placeholder="Paris, London, Rome..."
-                    onChange={this.handleInput}
-                    autoFocus
-                />
-                <button
-                    onClick={this.handleSubmission}    
-                >
-                    Search Weather
-                </button>
-            </div>
-        )
-    }
+const Search = ({ handleInput, handleSubmission }) => (
+    <div className={styles.container}>
+        <input 
+        placeholder="Paris, London, Rome..."
+        onChange={handleInput}
+        autoFocus
+        />
+        <button
+        onClick={handleSubmission}    
+        >
+        Search Weather
+        </button>
+    </div>
+)
+
+Search.propTypes = {
+    handleInput: PropTypes.func.isRequired,
+    handleSubmission: PropTypes.func.isRequired
 }
 
 export default Search;
--- app/containers/SearchContainer.jsx
+++ app/containers/SearchContainer.jsx
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import getWeather from '../utils/api';
 import Search from '../components/Search';
 
 class SearchContainer extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            search: ''
+        }
+        
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmission = this.handleSubmission.bind(this);
     }
     
-    componentDidMount() {
-        
+    handleInput(e) {
+        const val = e.target.value;
+        this.setState(() => ({search: val}));
+    }
+    
+    handleSubmission() {
+        const { search } = this.state;
+        getWeather(search)
+            .then((data) => console.log(data))
     }
     
     render() {
-        return <Search />;
+        return <Search 
+                    handleInput={this.handleInput}
+                    handleSubmission={this.handleSubmission}
+                />;
     }
 }
 
