f4ecc3c4aa915c99c564e6cc51edde3622874c21
James Robinson <jrobind@outlook.com>
2018-05-24 16:21:39 +0100

    reset stored weather data if previous search data already persisted

--- app/utils/storage.js
+++ app/utils/storage.js
@@ -2,6 +2,9 @@ export default {
     setStorage(data) {
         if (!localStorage.getItem('weather')) {
             localStorage.setItem('weather', JSON.stringify(data));
+        } else {
+            localStorage.removeItem('weather');
+            localStorage.setItem('weather', JSON.stringify(data));
         }
     },
     getStorage() {
