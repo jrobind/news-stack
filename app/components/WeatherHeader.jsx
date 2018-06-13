1bb3b0c00a3d296c853c9231aa2bc97e6b125a89
James Robinson <jrobind@outlook.com>
2018-06-13 15:34:32 +0100

    format api date for easier readability

--- app/components/WeatherHeader.jsx
+++ app/components/WeatherHeader.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from '../styles/components/WeatherHeader.scss';
+import Moment from 'react-moment';
 
 const WeatherHeader = ({ currentWeather, country, name, coord }) => (
     <div className={styles.container}>
         <div className={styles.placeInfo}>
             <p>{name}, {country}</p>
-            <p>{currentWeather.dt_txt}</p>
+            <p>
+                <Moment format='LL'>{currentWeather.dt_txt}</Moment>
+            </p>
         </div>
     </div>
 )
