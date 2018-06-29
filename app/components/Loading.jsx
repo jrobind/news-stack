d6ce07d91d9e80517d94a84b384639c855b2a34e
James Robinson <jrobind@outlook.com>
2018-06-29 13:42:00 +0100

    convert Loading to stateless functional component

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -1,18 +1,10 @@
 import React,{ Component } from 'react';
 import styles from '../styles/components/Loading.scss';
 
-class Loading extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className={styles.container}>
-                <div className={styles.loading}></div>
-            </div>
-        )
-    }
-}
+const Loading = () => (
+    <div className={styles.container}>
+        <div className={styles.loading}></div>
+    </div>
+)
 
 export default Loading;
