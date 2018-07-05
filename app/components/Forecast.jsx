644bd752cb392a24c419c584533993f084afed77
James Robinson <jrobind@outlook.com>
2018-07-05 14:09:43 +0100

    add Forecast component

---
+++ app/components/Forecast.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import iconCodes from '../utils/iconCodes';
+import styles from '../styles/components/Forecast.scss';
+
+const Forecast = ({}) => (
+    <div className={styles.container}>
+        
+    </div>
+)
+
+export default Forecast;
