c3a50012b032415419523f06469a7e109cd37a45
James Robinson <jrobind@outlook.com>
2018-07-10 15:40:04 +0100

    add new Forecast component tests

--- __tests__/Forecast-test.js
+++ __tests__/Forecast-test.js
@@ -7,30 +7,39 @@ import { apiFakeData } from '../testHelpers/fakeData';
 describe('<Forecast />', () => {
 
     it('should render a .container class', () => {
-        const wrapper = shallow(<Forecast forecast={[apiFakeData.list[0]]} />);
+        const wrapper = shallow(<Forecast forecast={apiFakeData.list} />);
         
         expect(wrapper.find('.container')).toHaveLength(1);
     });
 
     it('should render a .forecastWrapper class', () => {
-        const wrapper = shallow(<Forecast forecast={[apiFakeData.list[0]]} />);
+        const wrapper = shallow(<Forecast forecast={apiFakeData.list} />);
         
-        expect(wrapper.find('.forecastWrapper')).toHaveLength(1);
+        expect(wrapper.find('.forecastWrapper')).toHaveLength(2);
     });
 
     it('should render current weather conditions', () => {
-        const wrapper = shallow(<Forecast forecast={[apiFakeData.list[0]]} />);
+        const wrapper = shallow(<Forecast forecast={apiFakeData.list} />);
+        const forecastOne = wrapper.find('.description').at(0);
+        const forecastTwo = wrapper.find('.description').at(1);
 
-        expect(
-            wrapper
-                .find('.description')
-                .childAt(0)
-                .text()).toEqual('clear sky');
+        expect(forecastOne.text()).toEqual('clear sky');
+        expect(forecastTwo.text()).toEqual('scattered clouds');
     });
 
-    // it('should render <ForecastSelect /> component', () => {
-    //     const wrapper = shallow(<Forecast forecast={[apiFakeData.list[0]]} />);
+    it('should render correct temperature', () => {
+        const wrapper = shallow(<Forecast forecast={apiFakeData.list} />);
+        const forecastOne = wrapper.find('.temperature').at(0);
+        const forecastTwo = wrapper.find('.temperature').at(1);
 
-    //     expect(wrapper.find(ForecastSelect)).toHaveLength(1);
-    // });
+        expect(forecastOne.text()).toEqual('27℃');
+        expect(forecastTwo.text()).toEqual('37℃');
+    });
+
+    it('should render <ForecastSelect /> component for days other than today', () => {
+        const wrapper = shallow(<Forecast forecast={apiFakeData.list} />);
+
+        expect(wrapper.find(ForecastSelect)).toHaveLength(1);
+    });
+    
 });
