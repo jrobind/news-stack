bed133c87ed9059a77e6a4b7be1f7ca88c7766fe
James Robinson <jrobind@outlook.com>
2018-07-11 12:32:21 +0100

    add new ForecastSelect component tests

--- __tests__/ForecastSelect-test.js
+++ __tests__/ForecastSelect-test.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ForecastSelect from '../app/components/ForecastSelect';
 
+// mocks
+
+// mock for DOM Element.closest() and Element.getAttribute()
+const closest = () => ({ getAttribute() {} });
+// mock for updateForecast() function prop
+const updateForecastMock = jest.fn(() => ({
+    value: 'Monday', 
+    date: '2018-07-11 12:00:00'
+}));
+
+beforeEach(function() {
+    updateForecastMock.mockClear();
+  });
+
+// tests
+
 describe('<ForecastSelect />', () => {
 
     it('should render a .timeOfDay class', () => {
@@ -16,11 +32,18 @@ describe('<ForecastSelect />', () => {
         expect(wrapper.state('value')).toEqual('Afternoon');
     });
 
-    // it('should update time of day value when new option is selected', () => {
-    //     const wrapper = shallow(<ForecastSelect />);
-    //     wrapper.find('select').simulate('change', {target: { value: 'Morning'}});
+    it('should update time of day value when new option is selected', () => {
+        const wrapper = shallow(<ForecastSelect updateForecast={updateForecastMock} />);
+        wrapper.find('select').simulate('change', {target: { value: 'Morning', closest }});
 
-    //     expect(wrapper.state('value')).toEqual('Morning');
-    // });
+        expect(wrapper.state('value')).toEqual('Morning');
+    });
+
+    it('should call updateForecast() when new option is selected', () => {
+        const wrapper = shallow(<ForecastSelect updateForecast={updateForecastMock} />);
+        wrapper.find('select').simulate('change', {target: { value: 'Morning', closest }});
+
+        expect(updateForecastMock).toHaveBeenCalledTimes(1);
+    });
 
 });
