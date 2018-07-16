daccb0d6c13054a0efd7d3368dd660305afb5c90
James Robinson <jrobind@outlook.com>
2018-07-16 17:18:46 +0100

    add UV index call

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,12 @@
-export default ({lat, lng}) => {
+export default ({ lat, lng }) => {
    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=63a86f0f410d1650aed27460d8b457a4`)
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => console.log(error));
+}
+
+export const fetchUVIndex = ({ lat, lon }) => {
+   return fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=63a86f0f410d1650aed27460d8b457a4`)
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.log(error));
