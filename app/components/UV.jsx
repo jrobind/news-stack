f2a7c2e2f050619f20f1e1a1e44cb4dcb2e32b09
James Robinson <jrobind@outlook.com>
2018-07-18 18:39:03 +0100

    UV component dynamically renders UV index on click

--- app/components/UV.jsx
+++ app/components/UV.jsx
@@ -1,27 +1,76 @@
 import React, { Component } from 'react';
 import { fetchUVIndex } from '../utils/api';
 import storage from '../utils/storage';
+import { Line } from 'rc-progress';
 import styles from '../styles/components/UV.scss'; 
 
 class UV extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: true
+            value: null,
+            clicked: false
         }
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    componentDidMount() {
+    handleClick() {
         const { city: { coord } } = storage.getStorage('weather');
-        console.log(coord);
+
+        // set loading state to true while retrieving uv api data
+        this.setState(() => ({clicked: true}));
+
+        fetchUVIndex(coord)
+        .then(({ value }) => this.setState(() => ({value})))
+        .catch((error) => console.log(error))
+
     }
 
     render() {
+        const { clicked, value } = this.state;
+
         return (
-            <div className={styles.container}>
-                <h3>UV index</h3>
+            <div className={styles.container}>     
+                <div className={styles.uv}>
+                    <h2>UV Index</h2>
+                    <hr/>
+
+                    {clicked && 
+                        <div className={styles.uvAnimationContainer}>
+                            <p className={styles.index}>{Math.round(value)}</p>
+                            <Line 
+                                className={styles.progress}
+                                percent={value * 9} 
+                                strokeWidth="1.5"
+                                trailWidth="1.5"
+                                strokeColor="#19315b"
+                            />
+                        </div>
+                    }
+
+                    {!clicked && 
+                        <button 
+                            className={styles.seeIndex}
+                            onClick={this.handleClick}
+                        >
+                            See current index
+                        </button>
+                    }
+
+                    <p className={styles.uvDescription}>The UV index identifies the strength of the ultraviolet radiation from the sun. Large amounts of UV exposure at a high index can lead to serious health issues. Take necessary precautions to ensure you stay safe in the sun.</p>
+                </div>
             </div>
+
         )
+
+        if (loading) {
+            return (
+                <div className={styles.container}> 
+                    <Loading addClass={false} />
+                </div>
+            )    
+        } 
     }
 }
 
