aadc457403aa9ef7861dabce6719526d4ad9fa3c
James Robinson <jrobind@outlook.com>
2018-07-19 13:02:47 +0100

    update and refactor Loading component tests

--- __tests__/Loading-test.js
+++ __tests__/Loading-test.js
@@ -3,17 +3,23 @@ import { shallow } from 'enzyme';
 import Loading from '../app/components/Loading';
 
 describe('<Loading />', () => {
-    
-    it('should render a .container class', () => {
-        const wrapper = shallow(<Loading />);
-        
-        expect(wrapper.find('.container')).toHaveLength(1);
-    });
 
     it('should render a .loading class', () => {
         const wrapper = shallow(<Loading />);
         
         expect(wrapper.find('.loading')).toHaveLength(1);
+    });
+    
+    it('should render a .container class if passed truthy prop', () => {
+        const wrapper = shallow(<Loading addClass={true} />);
+        
+        expect(wrapper.find('.container')).toHaveLength(1);
+    });
+    
+    it('should not render a .container class if no prop is passed', () => {
+        const wrapper = shallow(<Loading />);
+        
+        expect(wrapper.find('.container')).toHaveLength(0);
     });
     
 });
