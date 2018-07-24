269b832b80500fd561415ee9ef45a641be0d53f4
James Robinson <jrobind@outlook.com>
2018-07-24 17:40:53 +0100

    update Tooltip component styles

--- app/utils/UVPollutionClassification.js
+++ app/utils/UVPollutionClassification.js
@@ -1,28 +1,28 @@
 export default (type, value) => {
     if (type === 'UV' && value < 2.9 || type === 'Pollution' && value <= 50) {
         return {
-            color: '#3fdb3f',
+            color: 'rgba(63, 219, 63, 0.8)',
             text: type === 'UV' ? 'Low' : 'Excellent' 
         }
     } else if (type === 'UV' && value > 2.9 && value <= 5.9 || type === 'Pollution' && value > 50 && value <= 100) {
         return {
-            color: '#ffbf00',
+            color: 'rgba(255, 191, 0, 0.8)',
             text: type === 'UV' ? 'Moderate' : 'Good' 
         }
     } else if (type === 'UV' && value > 5.9 && value <= 7.9 || type === 'Pollution' && value > 100 && value <= 150) {
         return {
-            color: '#e9692c',
-            text:  type === 'UV' ? 'High' : 'Lightly polluted' 
+            color: 'rgba(233, 105, 44, 0.8)',
+            text: 'High'  
         }
     } else if (type === 'UV' && value > 7.9 && value <= 10.9 || type === 'Pollution' && value > 150 && value <= 200) {
         return {
-            color: '#E50000',
-            text:  type === 'UV' ? 'Very high' : 'Moderately polluted' 
+            color: 'rgba(229, 0, 0, 0.8)',
+            text: 'Very high'  
         }
     } else if (type === 'UV' && value > 10.9 || type === 'Pollution' && value > 200) {
         return {
-            color: '#990000',
-            text:  type === 'UV' ? 'Extreme' : 'Heavily polluted' 
+            color: 'rgba(153, 0, 0, 0.8)',
+            text: 'Extreme'  
         }
     }
 }
