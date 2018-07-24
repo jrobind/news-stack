b28094ffeb58036af7da6a5ebdbd4ae3fd9c1e10
James Robinson <jrobind@outlook.com>
2018-07-24 13:46:45 +0100

    convert Tooltip component to functional component and include propTypes

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -1,21 +1,19 @@
 import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from '../styles/components/Tooltip.scss';
 
+const Tooltip = ({ data: { color, text}}) => (
+    <div className={styles.toolTip} style={{backgroundColor: color}}>
+        <p>{text}</p>
+        <div 
+            className={styles.tail} 
+            style={{borderTop: `10px solid ${color}`}}
+        ></div>
+    </div>
+)
 
-class Tooltip extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return(
-            <div className={styles.toolTip}>
-                <p>VERY HIGH</p>
-                <div className={styles.tail}></div>
-            </div>
-        )
-    }
-
+Tooltip.propTypes = {
+    data: PropTypes.object.isRequired
 }
 
 export default Tooltip;
