2030f4d2464ba97d0ad8fd8a10cd42b289b3d8a4
James Robinson <jrobind@outlook.com>
2018-08-27 13:14:21 +0100

    update util - UVandPollutionClassification() - test cases

--- __tests__/utils-test.js
+++ __tests__/utils-test.js
@@ -6,12 +6,16 @@ describe('UVandPollutionClassification', () => {
 
     it('should return correct rgb and text value for all UV cases', () => {
        const value_1 = UVandPollutionClassification('UV', 2.8);
-       const value_2 = UVandPollutionClassification('UV', 2.8);
-       const value_3 = UVandPollutionClassification('UV', 2.8);
-       const value_4 = UVandPollutionClassification('UV', 2.8);
-       const value_5 = UVandPollutionClassification('UV', 2.8);
+       const value_2 = UVandPollutionClassification('UV', 3.5);
+       const value_3 = UVandPollutionClassification('UV', 6.2);
+       const value_4 = UVandPollutionClassification('UV', 9);
+       const value_5 = UVandPollutionClassification('UV', 11.4);
 
-       expect(value).toMatchObject({color: 'rgba(63, 219, 63, 0.8)', text:'Low' });
+       expect(value_1).toMatchObject({color: 'rgba(63, 219, 63, 0.8)', text:'Low' });
+       expect(value_2).toMatchObject({color: 'rgba(255, 191, 0, 0.8)', text: 'Moderate'});
+       expect(value_3).toMatchObject({color: 'rgba(233, 105, 44, 0.8)', text: 'High'});
+       expect(value_4).toMatchObject({color: 'rgba(229, 0, 0, 0.8)', text: 'Very high'});
+       expect(value_5).toMatchObject({color: 'rgba(153, 0, 0, 0.8)', text: 'Extreme'});
     });
 
 });
