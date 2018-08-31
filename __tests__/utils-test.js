4caca4484808b35ca60a27c146fd0ce004cd78df
James Robinson <jrobind@outlook.com>
2018-08-31 21:08:13 +0100

    add new UVandPollution() util tests

--- __tests__/utils-test.js
+++ __tests__/utils-test.js
@@ -18,4 +18,18 @@ describe('UVandPollutionClassification', () => {
        expect(value_5).toMatchObject({color: 'rgba(153, 0, 0, 0.8)', text: 'Extreme'});
     });
 
+    it('should return correct rgb and text value for all Pollution index cases', () => {
+       const value_1 = UVandPollutionClassification('Pollution', 35);
+       const value_2 = UVandPollutionClassification('Pollution', 70);
+       const value_3 = UVandPollutionClassification('Pollution', 135);
+       const value_4 = UVandPollutionClassification('Pollution', 187);
+       const value_5 = UVandPollutionClassification('Pollution', 245);
+
+       expect(value_1).toMatchObject({color: 'rgba(63, 219, 63, 0.8)', text:'Excellent' });
+       expect(value_2).toMatchObject({color: 'rgba(255, 191, 0, 0.8)', text: 'Good'});
+       expect(value_3).toMatchObject({color: 'rgba(233, 105, 44, 0.8)', text: 'High'});
+       expect(value_4).toMatchObject({color: 'rgba(229, 0, 0, 0.8)', text: 'Very high'});
+       expect(value_5).toMatchObject({color: 'rgba(153, 0, 0, 0.8)', text: 'Extreme'});
+    });
+
 });
