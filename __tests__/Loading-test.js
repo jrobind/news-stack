a0fde6e4ffc2ce4da4c30e0a4db3f7bf9de8c2c0
James Robinson <jrobind@outlook.com>
2018-09-26 20:23:29 +0100

    update Loading component tests

--- __tests__/Loading-test.js
+++ __tests__/Loading-test.js
@@ -4,22 +4,16 @@ import Loading from '../app/components/Loading';
 
 describe('<Loading />', () => {
 
+    it('should render a .loadingWrapper class', () => {
+        const wrapper = shallow(<Loading />);
+        
+        expect(wrapper.find('.loadingWrapper')).toHaveLength(1);
+    });
+    
     it('should render a .loading class', () => {
         const wrapper = shallow(<Loading />);
         
         expect(wrapper.find('.loading')).toHaveLength(1);
-    });
-    
-    it('should render a .search class if passed truthy prop', () => {
-        const wrapper = shallow(<Loading addClass={true} />);
-        
-        expect(wrapper.find('.search')).toHaveLength(1);
-    });
-    
-    it('should not render a .dash class if no prop is passed', () => {
-        const wrapper = shallow(<Loading />);
-        
-        expect(wrapper.find('.container')).toHaveLength(0);
     });
     
 });
