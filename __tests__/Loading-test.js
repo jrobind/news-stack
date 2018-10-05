f7dbc11687d18dc30b7407bc85d07d3631a3fa89
James Robinson <jrobind@outlook.com>
2018-10-05 12:08:06 +0100

    finish Loading component UI test updates

--- __tests__/Loading-test.js
+++ __tests__/Loading-test.js
@@ -4,16 +4,16 @@ import Loading from '../app/components/Loading';
 
 describe('<Loading />', () => {
 
-    it('should render a .loadingWrapper class', () => {
+    it('should render a loading-wrapper data-testid attribute', () => {
         const wrapper = shallow(<Loading />);
         
-        expect(wrapper.find('.loadingWrapper')).toHaveLength(1);
+        expect(wrapper.find('[data-testid="loading-wrapper"]')).toHaveLength(1);
     });
     
-    it('should render a .loading class', () => {
+    it('should render a loading data-testid attribute', () => {
         const wrapper = shallow(<Loading />);
         
-        expect(wrapper.find('.loading')).toHaveLength(1);
+        expect(wrapper.find('[data-testid="loading"]')).toHaveLength(1);
     });
     
 });
--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -2,8 +2,14 @@ import React,{ Component } from 'react';
 import styles from '../styles/components/Loading.scss';
 
 const Loading = () => (
-    <div className={styles.loadingWrapper}>
-        <div className={styles.loading}></div>
+    <div 
+        className={styles.loadingWrapper}
+        data-testid='loading-wrapper'
+    >
+        <div 
+            className={styles.loading}
+            data-testid='loading'    
+        ></div>
     </div>
 )
 
