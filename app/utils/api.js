56418b389e982d6d7c2b98517cf678473b8e2b06
James Robinson <jrobind@outlook.com>
2018-12-05 16:18:05 +0000

    api request now served over HTTPS

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,19 +1,19 @@
 export default ({ lat, lng }) => {
-   return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=63a86f0f410d1650aed27460d8b457a4`)
+   return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=63a86f0f410d1650aed27460d8b457a4`)
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.log(error));
 }
 
 export const fetchUVIndex = ({ lat, lon }) => {
-   return fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=63a86f0f410d1650aed27460d8b457a4`)
+   return fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=63a86f0f410d1650aed27460d8b457a4`)
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.log(error));
 }
 
 export const fetchPollutionIndex = ({ lat, lon }) => {
-   return fetch(`http://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${lon}&key=NQWFi7DBJzeXq6m3t`)
+   return fetch(`https://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${lon}&key=NQWFi7DBJzeXq6m3t`)
         .then((response) => response.json())
         .then((data) => data)
         .catch((error) => console.log(error));
