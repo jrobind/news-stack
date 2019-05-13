8c4ade17b90d96270e88c6f2bcf7efa8391bf9c2
jrobind <jrobind@outlook.com>
2019-05-13 14:50:56 +0100

    fix broken tests

--- __tests__/DashboardContainer-test.js
+++ __tests__/DashboardContainer-test.js
@@ -77,7 +77,7 @@ describe('<DashboardContainer/>', () => {
         ).find(DashboardContainer).dive();
 
         const formattedMockData = apiMockData;
-        formattedMockData.forecast.forecastday = formattedMockData.forecast.forecastday.splice(1);
+        formattedMockData.forecast.forecastday = formattedMockData.forecast.forecastday;
        
         expect(wrapper.state('name')).toBe('Lodz');
         expect(wrapper.state('country')).toBe('Poland');
