2e80353c1272b1648c48c0331acb5012bc601ace
jrobind <jrobind@outlook.com>
2019-06-06 21:26:16 +0100

    remove old file reference that is breaking netlify build

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/base/base.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
